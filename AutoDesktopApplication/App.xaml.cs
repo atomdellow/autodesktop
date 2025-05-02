be50024b1ed1a6610d0639180a9ca22956122197
kendron2357 <atomdellow@gmail.com>
2025-05-02 00:08:52 -0400

    initial commit, using maui .net 8 app to create a desktop-agent

---
+++ AutoDesktopApplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoDesktopApplication
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
